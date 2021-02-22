b04d0ab26ce4a4ea38c0dc87f33cf4c581645267
Verhovetchi Daniel <daniel.verhovetchi@ati.utm.md>
2021-02-23 00:42:26 +0200

    Create VS project DIP

---
+++ DIP/DIP/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DIP
+{
+     class Player
+     {
+          public virtual void getSalary() => Console.WriteLine("Get salary method!!!");
+     }
+
+     class SimplePlayer : Player
+     {
+          public override void getSalary()
+          {
+               Salary salary = new Salary();
+               salary.Withdraw();
+          }
+     }
+
+     class ModernPlayer : Player
+     {
+          private Salary salary;
+
+          public ModernPlayer(Salary salary) => this.salary = salary;
+
+          public override void getSalary() => salary.Withdraw();
+     }
+
+     class theMostModernPlayer : Player
+     {
+          private IExchange salary;
+
+          public theMostModernPlayer(IExchange salary) => this.salary = salary;
+
+          public override void getSalary() => salary.Withdraw();
+     }
+}
---
+++ DIP/DIP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DIP
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               //Player player = new SimplePlayer();
+               //player.getSalary();
+
+               //Salary salary = new SalaryPound();
+               //Player player = new ModernPlayer(salary);
+               //player.getSalary();
+
+               //IExchange exchange = new SalaryEuro();
+               //Player player = new theMostModernPlayer(exchange);
+               //player.getSalary();
+
+               Console.ReadKey();
+          }
+     }
+}
