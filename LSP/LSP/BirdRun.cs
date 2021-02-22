c5a2404176d3ded0c3fdd1817f63993a26014dcd
Verhovetchi Daniel <daniel.verhovetchi@ati.utm.md>
2021-02-23 00:41:04 +0200

    Create VS project LSP

---
+++ LSP/LSP/BirdRun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViolLSP
+{
+     class BirdRun : Bird
+     {
+          private Bird bird;
+
+          public BirdRun(Bird bird)
+          {
+               this.bird = bird;
+          }
+
+          public void run()
+          {
+               FlySpeed = bird.Fly();
+          }
+     }
+}
---
+++ LSP/ViolLSP/PlayerRating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViolLSP
+{
+     class PlayerRating : Player
+     {
+          private Player player;
+
+          public PlayerRating(Player player)
+          {
+               this.player = player;
+          }
+
+          public void position()
+          {
+               Rating = player.playerRating();
+          }
+     }
+}
